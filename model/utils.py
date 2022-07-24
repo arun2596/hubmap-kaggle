58fe6f28a275b463229de4ff5f75d0a0f9287962
rarun2596 <rarun2596@gmail.com>
2022-07-24 15:23:43 -0700

    added seed for torch backend

--- model/utils.py
+++ model/utils.py
@@ -52,7 +52,8 @@ def set_seed(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
-    
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
     return
 
 
