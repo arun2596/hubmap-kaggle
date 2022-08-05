178ae1aff77d2f5edf32d34c0d15fabcf0671f35
rarun2596 <rarun2596@gmail.com>
2022-08-05 16:48:24 -0700

    mit b2 stained 40%

--- data_loader.py
+++ data_loader.py
@@ -42,7 +42,7 @@ class DatasetRetriever(Dataset):
         img_id, rle = self.img_id[item], self.rles[item]
 
         #adding stained images
-        stain_prob=0.2
+        stain_prob=0.4
         stain_flag=False
         if random.random()<stain_prob and self.mode=='train':
             stain_flag=True
